3995950d4949e34bf9cc411b348e9aa88e682e9f
Jeroen Steenkamp <j.steenkamp@ing.nl>
2016-05-14 13:42:42 +0200

    now returns a working xml document; requires modified royalts package
          object: function(parentguid, name, type, cb) {
            var guid = uuid.v4();
            var date = moment().format('YYYY/mm/DD H:MM:ss');
            var array = [
                { name: 'RoyalObjectType', value: type },
                { name: 'ID', value: guid },
                { name: 'Modified', value: date },
                { name: 'Name', value: name },
                { name: 'Created', value: date },
                { name: 'ParentID', value: parentguid }
            ];
            cb(array.concat(rtsd[type]), guid);

--- server.js
+++ server.js
@@ -19,10 +19,12 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-var doc = [];
-function addToArray(value) {
-  //doc = doc + ", " + value;
-  doc.push(value);
+var jsonObj = { "KeyValuePairs": [ ] };
+function addToJson(keypairs, uuid) {
+  for (i=0; i < keypairs.length; i++) {
+    //console.log(keypairs[i])
+    jsonObj.KeyValuePairs.push ({ "ObjectID": uuid, "Key": keypairs[i].name, "Value": keypairs[i].value });
+  }
 }
 
 app.post("/api", function(req, res) {
@@ -31,19 +33,19 @@ app.post("/api", function(req, res) {
     handleError(res, "Invalid input", "Must have a document attribute", 400);
   }
 
-  royalts.object("",req.body.name,"RoyalDocument",function (document,docuuid) {
-    addToArray(JSON.stringify(document));
-    //console.log(JSON.stringify(document));
+  royalts.object("",req.body.name,"RoyalDocument",function (document, docuuid) {
+    addToJson(document, docuuid);
+    //console.log(document);
     for (i=0;i < req.body.document[0].folders[0].folder.length;i++) {
       (function(counter) {
         royalts.object(docuuid, req.body.document[0].folders[0].folder[counter].name, "RoyalFolder", function (folder, folderuuid) {
-          addToArray(JSON.stringify(folder));
-          //console.log(JSON.stringify(folder));
+          addToJson(folder, folderuuid);
+          //console.log(folder);
           for (j = 0; j < req.body.document[0].folders[0].folder[counter].servers.length; j++) {
             var server = req.body.document[0].folders[0].folder[counter].servers[j];
             royalts.object(folderuuid,server.name, "RoyalSSHConnection", function (ssh, sshuuid) {
-              addToArray(JSON.stringify(ssh));
-              //console.log(JSON.stringify(ssh));
+              addToJson(ssh, sshuuid);
+              //console.log(ssh);
             })
           }
         });
@@ -51,9 +53,10 @@ app.post("/api", function(req, res) {
     }
   });
 
-  //royalts.savegenerate('[' + doc.join(', ') + ']', res, req.body.name + '.rtsz');
-  console.log(royalts.generate('[' + doc.join(', ') + ']'));
+  console.log(royalts.generate(jsonObj));
   res.status(200).json({ "status": "ok" });
+  //royalts.savegenerate(jsonObj, res, req.body.name + '.rtsz');
+
 });
 
 //{
