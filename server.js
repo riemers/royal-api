e1ed63e0b61ca162453ee5a12b5c94f16aab83f0
Erik-jan Riemers <erik-jan.riemers@ing.nl>
2016-05-20 15:13:01 +0200

    Changed to ES6, added babel for starting on node

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var royalts = require("royalts");
+import express from 'express';
+import bodyParser from 'body-parser';
+import royalts from 'royalts';
 
-var app = express();
-app.use(express.static(__dirname + "/public"));
+const app = express();
+app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
 
 // Initialize the app.
-var server = app.listen(process.env.PORT || 7000, function () {
-  var port = server.address().port;
+const server = app.listen(process.env.PORT || 7000, () => {
+  const port = server.address().port;
   console.log("App now running on port", port);
 });
 
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
+  console.log(`ERROR: ${reason}`);
   res.status(code || 500).json({"error": message});
 }
 
-var jsonObj = { "KeyValuePairs": [ ] };
+const jsonObj = { "KeyValuePairs": [ ] };
 function addToJson(keypairs, uuid) {
   for (i=0; i < keypairs.length; i++) {
     //console.log(keypairs[i])
@@ -27,36 +26,34 @@ function addToJson(keypairs, uuid) {
   }
 }
 
-app.post("/api", function(req, res) {
+app.post("/api", (req, res) => {
   //console.log(req.body);
   if (! req.body.name) {
     handleError(res, "Invalid input", "Must have a document attribute", 400);
   }
 
-  royalts.object("",req.body.name,"RoyalDocument",function (document, docuuid) {
+  royalts.object("",req.body.name,"RoyalDocument",(document, docuuid) => {
     addToJson(document, docuuid);
     //console.log(document);
     for (i=0;i < req.body.document[0].folders[0].folder.length;i++) {
-      (function(counter) {
-        royalts.object(docuuid, req.body.document[0].folders[0].folder[counter].name, "RoyalFolder", function (folder, folderuuid) {
+      ((counter => {
+        royalts.object(docuuid, req.body.document[0].folders[0].folder[counter].name, "RoyalFolder", (folder, folderuuid) => {
           addToJson(folder, folderuuid);
           //console.log(folder);
           for (j = 0; j < req.body.document[0].folders[0].folder[counter].servers.length; j++) {
-            var server = req.body.document[0].folders[0].folder[counter].servers[j];
-            royalts.object(folderuuid,server.name, "RoyalSSHConnection", function (ssh, sshuuid) {
+            const server = req.body.document[0].folders[0].folder[counter].servers[j];
+            royalts.object(folderuuid,server.name, "RoyalSSHConnection", (ssh, sshuuid) => {
               addToJson(ssh, sshuuid);
               //console.log(ssh);
             })
           }
         });
-      }(i));
+      })(i));
     }
   });
 
   console.log(royalts.savegenerate(jsonObj, res, req.body.name));
   res.status(200).json({ "status": "ok" });
-  //royalts.savegenerate(jsonObj, res, req.body.name + '.rtsz');
-
 });
 
 //{
