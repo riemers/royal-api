4e10d79c9c78d7ce0fad78bbe22cbd9412b57992
Erik-jan Riemers <riemers@binkey.nl>
2016-05-21 16:30:47 +0200

    Added start to simple input screen for easy use.

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ const server = app.listen(process.env.PORT || 7000, () => {
 });
 
 app.post('/api', (req, res) => {
+  console.log(res.body);
   royalDocument.setRaw(req.body);
   try {
     res.set('Content-Type', 'text/xml');
@@ -23,86 +24,49 @@ app.post('/api', (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname +  '/input.html');
+});
 
 // const stuff = {
-//   Type: 'RoyalDocument',
-//   Name: 'Document Name',
-//   Children: [
-//     {
-//       Type: 'RoyalFolder',
-//       Name: 'production',
-//       Children: [
-//         {
-//           Type: 'RoyalSSHConnection',
-//           Name: 'coolserver',
-//           URI: '10.20.30.40',
-//         },
-//       ],
-//     },
-//
-//     {
-//       Type: 'RoyalFolder',
-//       Name: 'shitstick',
-//       Children: [
-//         {
-//           Type: 'RoyalFolder',
-//           Name: 'rgdfgdfg',
-//           Children: [
-//             {
-//               Type: 'RoyalSSHConnection',
-//               Name: 'harro',
-//               URI: '10.20.30.47',
-//             },
-//           ],
-//         },
-//         {
-//           Type: 'RoyalFolder',
-//           Name: 'ssh',
-//         },
-//       ],
-//     },
-//
-//     {
-//       Type: 'RoyalFolder',
-//       Name: 'dev',
-//       Children: [
-//         {
-//           Type: 'RoyalFolder',
-//           Name: 'secret stuff',
-//           Children: [
-//             {
-//               Type: 'RoyalSSHConnection',
-//               Name: 'secretServer',
-//               URI: '10.20.30.47',
-//             },
-//           ],
-//         },
-//         {
-//           Type: 'RoyalFolder',
-//           Name: 'test',
-//           Children: [{
-//             Type: 'RoyalFolder',
-//             Name: 'shitstick',
-//             Children: [
-//               {
-//                 Type: 'RoyalFolder',
-//                 Name: 'rgdfgdfg',
-//                 Children: [
-//                   {
-//                     Type: 'RoyalSSHConnection',
-//                     Name: 'harro',
-//                     URI: '10.20.30.47',
-//                   },
-//                 ],
-//               },
-//               {
-//                 Type: 'RoyalFolder',
-//                 Name: 'ssh',
-//               },
-//             ],
-//           }],
-//         },
-//       ],
-//     },
-//   ],
-// };
+// "Type": "RoyalDocument",
+//   "Name": "Document Name",
+//   "Children": [{
+//   "Type": "RoyalFolder",
+//   "Name": "production",
+//   "Children": [{
+//     "Type": "RoyalSSHConnection",
+//     "Name": "coolserver",
+//     "URI": "10.20.30.40"
+//   }]
+// }, {
+//   "Type": "RoyalFolder",
+//   "Name": "something",
+//   "Children": [{
+//     "Type": "RoyalFolder",
+//     "Name": "inside something",
+//     "Children": [{
+//       "Type": "RoyalSSHConnection",
+//       "Name": "coolserver2",
+//       "URI": "10.20.30.47"
+//     }]
+//   }, {
+//     "Type": "RoyalFolder",
+//     "Name": "Secret stash"
+//   }]
+// }, {
+//   "Type": "RoyalFolder",
+//   "Name": "dev",
+//   "Children": [{
+//     "Type": "RoyalFolder",
+//     "Name": "secret stuff",
+//     "Children": [{
+//       "Type": "RoyalSSHConnection",
+//       "Name": "secretServer",
+//       "URI": "10.20.30.47"
+//     }]
+//   }, {
+//     "Type": "RoyalFolder",
+//     "Name": "Just my test"
+//   }]
+// }]
