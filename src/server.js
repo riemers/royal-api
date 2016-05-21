f5ca2fa25beff8d1d09d5cd3a940cc3574c1b5c1
Salakar <mike.diarmid@gmail.com>
2016-05-21 01:36:29 +0100

    res return the entity that is invalid/errored

--- src/server.js
+++ src/server.js
@@ -1,38 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import RoyalDocument from 'royalts';
+const royalDocument = new RoyalDocument();
 
 const app = express();
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
 
-// Initialize the app.
 const server = app.listen(process.env.PORT || 7000, () => {
   const port = server.address().port;
-  console.log("App now running on port", port);
+  console.log('App now running on port', port);
 });
 
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-  console.log(`ERROR: ${reason}`);
-  res.status(code || 500).json({ "error": message });
-}
-
 app.post('/api', (req, res) => {
-  const newRoyalDoc = new RoyalDocument(req.body);
-
+  royalDocument.setRaw(req.body);
   try {
     res.set('Content-Type', 'text/xml');
-    res.send(newRoyalDoc.toString());
+    res.send(royalDocument.toString());
   } catch (e) {
     res.status(400);
-    res.json({ error: e.message });
+    res.json({ error: e.message, entity: royalDocument.errorEntity });
   }
 });
 
-// folders
-// folder
-// terminal <-> servers
 
 // const stuff = {
 //   Type: 'RoyalDocument',
